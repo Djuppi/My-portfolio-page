4940d7c2ce5763681495fd5fa10891c24e54cc0d
Aske <askeda@gmail.com>
2020-01-21 12:05:34 +0100

    Updates

---
+++ precache-manifest.78abc86907918c7600996717e8ba5247.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b07098fa5b776463e7274b48877650d",
+    "url": "/My-portfolio-page/index.html"
+  },
+  {
+    "revision": "396848acaa1a7cb14926",
+    "url": "/My-portfolio-page/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d375a639996a1ae54b94",
+    "url": "/My-portfolio-page/static/css/main.62cf4c34.chunk.css"
+  },
+  {
+    "revision": "396848acaa1a7cb14926",
+    "url": "/My-portfolio-page/static/js/2.05e3b5a8.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/My-portfolio-page/static/js/2.05e3b5a8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d375a639996a1ae54b94",
+    "url": "/My-portfolio-page/static/js/main.df2cbb5e.chunk.js"
+  },
+  {
+    "revision": "62e49817dfe33620abd7",
+    "url": "/My-portfolio-page/static/js/runtime-main.1ebc6021.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/My-portfolio-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
